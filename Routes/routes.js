300303f3644cacb6c717adf3217775dba6c7a5e8
Suraj Singh <absurajsinghchamiyal@gmail.com>
2023-07-14 00:17:59 +0530

    set random interval for checking mailbox

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,35 +2,47 @@ const express = require("express");
 const router = express.Router();
 const { google } = require("googleapis");
 const { authentication, getUnreadEmails, createLabels, getEmailById, sendReply } = require("../Utils/Functions");
+
 router.get("/", async (req, res) => {
   try {
     //Authenticating User
     const auth = await authentication();
+
     if (auth) {
       //If auth is not null
       //Initialize gmail instance
       const gmail = google.gmail({ version: "v1", auth });
 
-      //Array of unread messages
-      const messages = await getUnreadEmails(gmail);
-      console.log(messages);
       //Create Label | Parameter : gmail instance, label name
       const labelId = await createLabels(gmail, "Autoresponder");
 
-      messages?.forEach(async (email) => {
-        const { id } = email;
-        //Fetch Email data
-        const emailData = await getEmailById(gmail, id);
-        //Template of reply message
-        const reply = "Thank you for reaching out. I'm currently busy, but I'll get back to you via email soon.";
-        //Send Reply
-        sendReply(gmail, emailData, reply, labelId);
-      });
+      //Repeat The Process after Random Interval
+      setInterval(async () => {
+        //Array of unread messages
+        const messages = await getUnreadEmails(gmail);
+
+        messages?.forEach(async (email) => {
+          const { id } = email;
+
+          //Fetch Email data
+          const emailData = await getEmailById(gmail, id);
+
+          //Checking if email has been replied before or not
+          const isReplied = emailData.payload.headers.some((header) => header.name === "In-Reply-To");
+
+          if (isReplied == false) {
+            //Template of reply message
+            const reply = "Thank you for reaching out. I'm currently busy, but I'll get back to you via email soon.";
+
+            //Send Reply
+            sendReply(gmail, emailData, reply, labelId);
+          }
+        });
+      }, Math.floor(Math.random() * (120 - 45 + 1) + 45) * 1000);
     }
   } catch (err) {
     console.log(err);
   }
-
   res.send({ msg: "Welcome to world !" });
 });
 
